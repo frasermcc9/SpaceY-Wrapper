36c191aa3057f3916a438a28ec2bcf501ae33eb8
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-18 21:19:03 +1200

    driver

---
+++ index.js
@@ -0,0 +1,21 @@
+const { createClient, Setup } = require("./build/index");
+
+(async () => {
+	const client = await createClient("202917897176219648");
+
+	try {
+		const data = await client.action("warp", "Kalen");
+		console.log(data.msg);
+		console.log(JSON.parse(data.player));
+	} catch (e) {
+		console.log(e);
+	}
+
+	client.on("err", (msg) => {
+		console.log(msg);
+	});
+
+	client.on("res", (msg) => {
+		console.log(msg);
+	});
+})();
